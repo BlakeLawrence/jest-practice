1f8c8cc27033c36ab07ffde16925b8e997b027ec
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2023-03-03 16:39:48 +0000

    added new tests

--- sum.test.js
+++ sum.test.js
@@ -11,4 +11,38 @@ describe("Sum function", () => {
   });
 });
 
-describe("truthy or falsy tests", () => {});
+//basic truthy testing concept
+describe("truthy or falsy tests", () => {
+  it("null", () => {
+    const n = null;
+    expect(n).toBeFalsy();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeNull();
+  });
+});
+
+// basic number testing
+describe("numbers testing", () => {
+  it("two plus two", () => {
+    const value = 2 + 2;
+    expect(value).toBe(4);
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeGreaterThanOrEqual(4);
+  });
+});
+
+describe("adding floats", () => {
+  it("0.1 + 0.2", () => {
+    const value = 0.1 + 0.2;
+    expect(value).toBeCloseTo(0.3);
+  });
+});
+
+// Strings
+describe("String tests", () => {
+  it("0.1 + 0.2", () => {
+    const value = 0.1 + 0.2;
+    expect(value).toBeCloseTo(0.3);
+  });
+});
